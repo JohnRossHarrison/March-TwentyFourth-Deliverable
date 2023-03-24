855824278c0a8038575e70fa2ac26b8d31506222
JohnRossHarrison <127551022+JohnRossHarrison@users.noreply.github.com>
2023-03-24 16:37:39 -0500

    adding comments to files

--- vpc.tf
+++ vpc.tf
@@ -8,9 +8,11 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
+  // Define the vpc name and reserve its CIDR block
   name = "example-vpc"
   cidr = "10.0.0.0/16"
 
+  // Assign the vpc's AZs and subnets
   azs             = ["us-west-2a", "us-west-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
