550c400b197c0447e22b4685acc1f7056dd1a6a3
JohnRossHarrison <127551022+JohnRossHarrison@users.noreply.github.com>
2023-03-24 11:23:48 -0500

    set up vpc and ec2 instance

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+  required_version = ">= 1.4.2"
+
+  cloud {
+    organization = "JohnRossHarrison"
+
+    workspaces {
+      name = "training-workspace"
+    }
+  }
+}
