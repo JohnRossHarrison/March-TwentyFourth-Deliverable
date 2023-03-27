6e601e8209530e28941bb878a090ea73167c113a
JohnRossHarrison <127551022+JohnRossHarrison@users.noreply.github.com>
2023-03-27 10:34:06 -0500

    commented out vpc

--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,7 @@
  * This module creates a VPC with the CIDR block 10.0.0.0/16 over the
  *  us-west-2 a & b availability zones, with 3 private and 3 public subnets
 **/
+/**
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -17,6 +18,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
+**/
 
 module "ec2_instances" {
   source = "./modules/ec2"
